ced5e3570308fdd8eb9f0440da94b7e7038ee733
turalbabayev <turalbabayev@outlook.com.tr>
2024-07-12 17:15:29 +0300

    Initial Commit

---
+++ Github API Calls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Github API Calls
+//
+//  Created by Tural Babayev on 12.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Github API Calls/Github_API_CallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_API_CallsApp.swift
+//  Github API Calls
+//
+//  Created by Tural Babayev on 12.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Github_API_CallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
